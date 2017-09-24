e2ebbaa4a8e0922d1a51c6d30df6c89a0ef101f8
omegadeep10 <omegadeep10@gmail.com>
2017-09-24 13:37:27 -0400

    Add some comments

--- api.py
+++ api.py
@@ -7,9 +7,11 @@ app.config['SECRET_KEY'] = 'tacos_21' # used for signing tokens
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_team:fudge1960@45.55.81.224/slo?charset=utf8&use_unicode=0'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # we don't use track mods, this gets rid of an error in the console
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600) # default to JWT expiration of one hour
+app.config['JWT_AUTH_USERNAME_KEY'] = 'email' # we use email, not the default 'username' field that flask_JWT expects
 
 api = Api(app)
 
+#initialize flask_JWT
 from flask_jwt import JWT, jwt_required, current_identity
 from controllers.auth import authenticate, identity, checkadmin
 jwt = JWT(app, authenticate, identity)
@@ -33,4 +35,5 @@ api.add_resource(Assessments,'/assessments') #<base_url>/assessments
 api.add_resource(Classes,'/classes')         #<base_url>/classes
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True) # runs on an internal ip at port 3000 that codeanywhere's linux box automatically maps to external IP
+                                                   # To get the external IP, right click on 'python-backend' and click info to view relevant details
--- controllers/auth.py
+++ controllers/auth.py
@@ -5,14 +5,20 @@ from flask_restful import abort
 from functools import wraps
 import sys
 
-
+# The authentication handler used by flask_JWT. Takes an email and password
 def authenticate(email, password):
+  #Query the database and return the ONE user that has the matching email/password. If user doesn't exist, return None
   return session.query(User).filter(User.email == email, User.password == password).one_or_none()
 
+
+# payload = JWT token sent by the user (as a header attribute)
+# gets the ID stored within the token and returns the user object using the id to query the database
 def identity(payload):
   user_id = payload['identity']
   return session.query(User).filter(User.id == user_id).one_or_none()
 
+
+# wrapper function that checks that the currently logged in user is an admin. If not, aborts with a 401 status code
 def checkadmin(func):
   @wraps(func)
   def wrapper(*args, **kwargs):

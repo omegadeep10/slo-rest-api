2a1e112728e132056af16fd142338bb27649446a
omegadeep10 <omegadeep10@gmail.com>
2017-10-03 19:02:17 -0400

    Add profile endpoint

---
+++ controllers/profile.py
@@ -0,0 +1,21 @@
+from flask_jwt import jwt_required, current_identity
+from flask_restful import Resource, fields, marshal_with, abort, reqparse
+from controllers.auth import checkadmin
+from db import session
+from models.User import UserModel
+from datetime import datetime
+
+user_fields = {
+  'email': fields.String,
+  'faculty_id': fields.String,
+  'first_name': fields.String,
+  'last_name': fields.String,
+  'user_type': fields.String
+}
+
+class Profile(Resource):
+  @jwt_required()
+  @marshal_with(user_fields)
+  def get(self):
+    return current_identity
+  

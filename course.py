ba836f40b108a9b61158bf70cd9dc56396331681
astrogirl133 <fire-fox-9@hotmail.com>
2017-10-03 23:11:19 -0400

    Updated the course model with the date datatype

--- course.py
+++ course.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy import Date
+from sqlalchemy import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()

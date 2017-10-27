e88941e43fef34c31b5739035b284a0bcc9cc91b
omegadeep10 <omegadeep10@outlook.com>
2017-10-27 16:29:59 -0400

    Add reporting functionality

--- controllers/report.py
+++ controllers/report.py
@@ -3,33 +3,122 @@ from flask_restful import Resource, reqparse, marshal_with, fields, abort
 from controllers.auth import checkadmin
 from db import session
 import xlsxwriter
-from models import AssessmentModel, PerfIndicatorModel, ScoreModel, SLOModel
+from models import AssessmentModel, PerfIndicatorModel, ScoreModel, CourseModel, SLOModel
+
+def getScoreCount(ListOfAssessments, desiredScore, performanceIndicatorId):
+    # scores => should be a list of score objects
+    # desiredScore => 1, 2, 3, or 4
+    count = 0
+
+    for assessment in ListOfAssessments:
+        for score in assessment.scores:
+            if (score.performance_indicator_id == performanceIndicatorId) and (score.score == desiredScore):
+                count = count + 1
+    
+    return count
+
+def safeDivision(x, y):
+    return 0 if x == 0 else x / y
+
+# Responsible for generating first sheet and populating with raw user data
+def generateRawData(excelWorkbook, course):
+    worksheet = excelWorkbook.add_worksheet('RawData') #adds a worksheet to the workbook
+    row, column = 0, 0
+    
+    for assignedSlo in course.assigned_slos:
+        assessments = session.query(AssessmentModel).filter(AssessmentModel.crn == course.crn, AssessmentModel.slo_id == assignedSlo.slo_id).all() #queries the tables for information
+
+        # CRN top left, SLO description right next to it
+        worksheet.write(row, column, course.crn) #adds data to the worksheet going by row and column numbers then the actual data
+        worksheet.write(row, column + 1, assignedSlo.slo_id + " : " + assignedSlo.slo.slo_description)
+        
+        # generate header row (Student, PI 1, PI 2, etc.)
+        row = row + 1
+        worksheet.write(row, column, 'Student')
+        column = column + 1
+
+        for performance_indicator in assignedSlo.slo.performance_indicators:
+            worksheet.write(row, column, performance_indicator.performance_indicator_description)
+            column = column + 1
+        
+        row = row + 1
+        column = 0
+
+        # Output data below the header (Student_id, score for PI 1, Score for PI 2, Score for PI 3)
+        for assessment in assessments:
+            worksheet.write(row, column, assessment.student_id)
+            column = column + 1
+
+            for score in assessment.scores:
+                worksheet.write(row, column, score.score)
+                column = column + 1
+            
+            column = 0
+            row = row + 1
+
+        # Empty row between raw data and summary statistics
+        row = row + 1
+        column = 0
+
+        # Header row for Summary data
+        worksheet.write(row, column, 'Number of students who scored...')
+        column = column + 1
+
+        for performance_indicator in assignedSlo.slo.performance_indicators:
+            worksheet.write(row, column, performance_indicator.performance_indicator_description)
+            column = column + 1
+        
+        row = row + 1
+        column = 0
+
+        # Summary data
+        for scoreRating in ([4, 'Exemplary'], [3, 'Satisfactory'], [2, 'Developing'], [1, 'Unsatisfactory']):
+            worksheet.write(row, column, scoreRating[1])
+            column = column + 1
+            
+            for performance_indicator in assignedSlo.slo.performance_indicators:
+                worksheet.write(row, column, getScoreCount(assessments, scoreRating[0], performance_indicator.performance_indicator_id))
+                column = column + 1
+            
+            row = row + 1
+            column = 0
+        
+        # Empty row between summary and totals
+        row = row + 1
+
+        # Totals for Summary
+        worksheet.write(row, column, 'Total')
+        column = column + 1
+
+        for performance_indicator in assignedSlo.slo.performance_indicators:
+            worksheet.write(row, column, len(assessments))
+            column = column + 1
+        
+        row = row + 1
+        column = 0
+
+        worksheet.write(row, column, 'Percent who scored either Satisfactory or Exemplary')
+        column = column + 1
+
+        for performance_indicator in assignedSlo.slo.performance_indicators:
+
+            percentForExemplary = safeDivision(getScoreCount(assessments, 4, performance_indicator.performance_indicator_id), len(assessments))
+            percentForSatisfactory = safeDivision(getScoreCount(assessments, 3, performance_indicator.performance_indicator_id), len(assessments))
+            worksheet.write(row, column, percentForExemplary + percentForSatisfactory)
+            column = column + 1
+
+
+        # Two empty rows for separation between classes
+        row = row + 3
+        column = 0
+
 
 class Report(Resource):
 
     def get(self,crn):
-        assessment = session.query(AssessmentModel).filter(AssessmentModel.crn == crn).first() #queries the tables for information
-        slos = session.query(SLOModel).filter(SLOModel.slo_id == assessment.slo_id).first()
-        scores = session.query(ScoreModel).filter(ScoreModel.assessment_id == AssessmentModel.assessment_id).first()
+        course = session.query(CourseModel).filter(CourseModel.crn == crn).one_or_none()
 
         workbook = xlsxwriter.Workbook('slos.xlsx') #creates the workbook and names it
-        worksheet = workbook.add_worksheet('SLOS') #adds a worksheet to the workbook
-
-        indicators = [] #intliazes a list
-        for performance_indicator in slos.performance_indicators: 
-            indicators.append(performance_indicator)  #loops through each performance indicator and adds it to the list
-
-        worksheet.write(0, 0, 'CRN') #adds data to the worksheet going by row and column numbers then the actual data
-        worksheet.write(0, 1, 'Student ID')
-        worksheet.write(0, 2, 'SLO ID')
-        worksheet.write(0, 3, "SLO Description")
-        worksheet.write(0, 4, 'Performance Indicator')
-        worksheet.write(0, 5, 'Score')
-        worksheet.write(1, 0, assessment.crn)
-        worksheet.write(1, 1, assessment.student_id)
-        worksheet.write(1, 2, assessment.slo_id)
-        worksheet.write(1, 3, assessment.slo.slo_description)
-        worksheet.write(1, 4, indicators[0].performance_indicator_description)
-        worksheet.write(1, 5, scores.score)
+        generateRawData(workbook, course)
 
         workbook.close() #closes the workbook
